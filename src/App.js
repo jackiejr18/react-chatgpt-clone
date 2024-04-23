27f9127ea0ba6afd312e92009628a400128dac20
Jackie Jr <jwysinger@gmail.com>
2024-04-23 12:29:11 -0500

    added assets folder with images and midified app css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import gptLogo from './assets/chatgpt.svg';
 
 function App() {
   return (
     <div className="App">
-      
-      
+      <div className='sidebar'>
+        <div className='upperSide'>
+          <div className='upperSideTop'><img src={gptLogo} className='logo'/><span className='brand'>ChatGPT</span></div>
+          <button className='midBtn'><img className='addBtn'/>New Chat</button>
+          <div className='upperSideBottom'>
+            <button className='query'><img src=''/>What is Programming ?</button>
+            <button className='query'><img src=''/>What is Programming ?</button>
+          </div>
+        </div>
+        <div className='lowerSide'>
+         
+        </div>
+      </div>
+      <div className='main'>
+
+      </div>
     </div>
   );
 }
